8229221dc27cc7716a82887e8697bd227f7f30b4
Ahmed <tanvirahmed754575@gmail.com>
2026-08-07 22:35:08 +0200

    database

--- src/database/db.py
+++ src/database/db.py
@@ -5,9 +5,9 @@ from src.config.settings import settings
 engine = create_engine(
     settings.DATABASE_URL, 
     connect_args={"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {},
-    poolclass=StaticPool
-    if "sqlite" in settings.DATABASE_URL
-    else None
+    # poolclass=StaticPool
+    # if "sqlite" in settings.DATABASE_URL
+    # else None
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
