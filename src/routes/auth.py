e9952b5bb1edb7430f1ccfaaaf3dc378a0e44378
Ahmed <tanvirahmed754575@gmail.com>
2026-08-07 22:35:24 +0200

    add get_user

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -24,12 +24,6 @@ def hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(pwd_bytes, salt).decode("utf-8")
 
-# def verify_password(plain_password: str, hashed_password: str) -> bool:
-#     if not plain_password or not hashed_password:
-#         return False
-#     pwd_bytes = plain_password.encode("utf-8")[:72]
-#     hashed_bytes = hashed_password.encode("utf-8")
-#     return bcrypt.checkpw(pwd_bytes, hashed_bytes)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     if not plain_password or not hashed_password:
